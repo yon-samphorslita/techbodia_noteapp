530e2096b07e5bd8f590fc7c842f7ace85754313
Yon Samphorslita <yon.samphorslita@gmail.com>
2025-07-09 16:35:25 +0700

    updated program.cs

--- NoteAppApi/Program.cs
+++ NoteAppApi/Program.cs
@@ -4,8 +4,6 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-// builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,8 +13,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowVueApp", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "https://yon-samphorslita.github.io"
-) 
+        policy.WithOrigins("http://localhost:5173", "https://yon-samphorslita.github.io")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
@@ -30,10 +27,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-// app.UseHttpsRedirection();
-app.UseAuthorization();
+
+app.UseHttpsRedirection();
+
 app.UseCors("AllowVueApp");
+
+app.UseAuthorization();
+
 app.MapControllers();
+
 app.MapGet("/", () => "Note API is running!");
 
 app.Run();
